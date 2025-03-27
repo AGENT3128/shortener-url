1a9e1f072dd773540c0004a1b4d2a77b370abb44
Sergey Macheret <smacheret@sharxdc.ru>
2025-03-27 11:28:16 +0300

    update arch

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,25 +2,10 @@ package main
 
 import (
 	"github.com/AGENT3128/shortener-url/internal/app/config"
-	"github.com/AGENT3128/shortener-url/internal/app/handlers"
 	"github.com/AGENT3128/shortener-url/internal/app/logger"
 	"github.com/AGENT3128/shortener-url/internal/app/server"
-	"github.com/AGENT3128/shortener-url/internal/app/storage"
 	"go.uber.org/zap"
 )
-
-type Repository interface {
-	ShortenerSet
-	ShortenerGet
-}
-
-type ShortenerSet interface {
-	Add(shortID, originalURL string)
-}
-type ShortenerGet interface {
-	GetByShortID(shortID string) (string, bool)
-	GetByOriginalURL(originalURL string) (string, bool)
-}
 
 func main() {
 	if err := run(); err != nil {
@@ -34,25 +19,9 @@ func run() error {
 		return err
 	}
 
-	var repository Repository
-	if cfg.FileStoragePath != "" {
-		repo, err := storage.NewFileStorage(cfg.FileStoragePath, logger.Log)
-		if err != nil {
-			return err
-		}
-		defer repo.Close()
-		repository = repo
-	} else {
-		repository = storage.NewMemStorage(logger.Log)
-	}
-
 	server, err := server.NewServer(
-		server.WithMode(cfg.ReleaseMode),
-		server.WithServerAddress(cfg.ServerAddress),
-		server.WithBaseURL(cfg.BaseURLAddress),
-		server.WithHandler(handlers.NewShortenHandler(repository, cfg.BaseURLAddress, logger.Log)),
-		server.WithHandler(handlers.NewRedirectHandler(repository, logger.Log)),
-		server.WithHandler(handlers.NewAPIShortenHandler(repository, cfg.BaseURLAddress, logger.Log)),
+		server.WithConfig(cfg),
+		server.WithLogger(logger.Log),
 	)
 	if err != nil {
 		return err
--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -5,8 +5,13 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/AGENT3128/shortener-url/internal/app/config"
+	"github.com/AGENT3128/shortener-url/internal/app/handlers"
+	"github.com/AGENT3128/shortener-url/internal/app/logger"
 	"github.com/AGENT3128/shortener-url/internal/app/middleware"
+	"github.com/AGENT3128/shortener-url/internal/app/storage"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 type IHandler interface {
@@ -27,43 +32,48 @@ type IHandlerFunc interface {
 	Handler() gin.HandlerFunc
 }
 
+type Repository interface {
+	ShortenerSet
+	ShortenerGet
+}
+
+type ShortenerSet interface {
+	Add(shortID, originalURL string)
+}
+type ShortenerGet interface {
+	GetByShortID(shortID string) (string, bool)
+	GetByOriginalURL(originalURL string) (string, bool)
+}
 type Server struct {
 	httpServer *http.Server
 	router     *gin.Engine
 	handlers   []IHandler
 }
 type options struct {
-	ginMode       string
-	serverAddress string
-	baseURL       string
-	handlers      []IHandler
+	config   *config.Config
+	logger   *zap.Logger
+	repo     Repository
+	handlers []IHandler
 }
 type Option func(options *options) error
-
-func WithMode(mode string) Option {
-	return func(o *options) error {
-		o.ginMode = mode
-		return nil
-	}
-}
-
-func WithServerAddress(address string) Option {
-	return func(o *options) error {
-		o.serverAddress = address
-		return nil
-	}
-}
-
-func WithBaseURL(baseURL string) Option {
-	return func(o *options) error {
-		o.baseURL = baseURL
-		return nil
-	}
-}
 
 func WithHandler(handler IHandler) Option {
 	return func(o *options) error {
 		o.handlers = append(o.handlers, handler)
+		return nil
+	}
+}
+
+func WithConfig(config *config.Config) Option {
+	return func(o *options) error {
+		o.config = config
+		return nil
+	}
+}
+
+func WithLogger(logger *zap.Logger) Option {
+	return func(o *options) error {
+		o.logger = logger
 		return nil
 	}
 }
@@ -76,8 +86,15 @@ func NewServer(opts ...Option) (*Server, error) {
 		}
 	}
 
+	if options.config == nil {
+		return nil, fmt.Errorf("config is nil")
+	}
+	if options.logger == nil {
+		return nil, fmt.Errorf("logger is nil")
+	}
+
 	// Set Gin mode
-	switch options.ginMode {
+	switch options.config.ReleaseMode {
 	case "debug":
 		gin.SetMode(gin.DebugMode)
 	case "release":
@@ -85,7 +102,18 @@ func NewServer(opts ...Option) (*Server, error) {
 	case "test":
 		gin.SetMode(gin.TestMode)
 	default:
-		return nil, fmt.Errorf("invalid release mode: %s", options.ginMode)
+		return nil, fmt.Errorf("invalid release mode: %s", options.config.ReleaseMode)
+	}
+
+	if options.config.FileStoragePath != "" {
+		repo, err := storage.NewFileStorage(options.config.FileStoragePath, options.logger)
+		if err != nil {
+			return nil, err
+		}
+		defer repo.Close()
+		options.repo = repo
+	} else {
+		options.repo = storage.NewMemStorage(logger.Log)
 	}
 
 	// Create router and setup middleware
@@ -96,13 +124,17 @@ func NewServer(opts ...Option) (*Server, error) {
 	router.Use(middleware.GzipMiddleware())
 
 	// Setup handlers
+	options.handlers = append(options.handlers, handlers.NewShortenHandler(options.repo, options.config.BaseURLAddress, options.logger))
+	options.handlers = append(options.handlers, handlers.NewRedirectHandler(options.repo, options.logger))
+	options.handlers = append(options.handlers, handlers.NewAPIShortenHandler(options.repo, options.config.BaseURLAddress, options.logger))
+
 	for _, handler := range options.handlers {
 		router.Handle(handler.Method(), handler.Pattern(), handler.Handler())
 	}
 
 	return &Server{
 		httpServer: &http.Server{
-			Addr:              options.serverAddress,
+			Addr:              options.config.ServerAddress,
 			Handler:           router,
 			ReadTimeout:       10 * time.Second,
 			WriteTimeout:      10 * time.Second,
