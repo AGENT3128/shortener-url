bbfd7688370f35ce97fb9bb8731034a0c82ea979
Sergey Macheret <smacheret@sharxdc.ru>
2025-06-09 12:17:45 +0300

    fix linter

--- internal/repository/file/file_repositories_test.go
+++ internal/repository/file/file_repositories_test.go
@@ -76,7 +76,7 @@ func TestBasicOperations(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+			ctx, cancel := context.WithTimeout(t.Context(), 2*time.Second)
 			defer cancel()
 
 			shortURL, err := ts.storage.Add(ctx, tt.userID, tt.shortID, tt.originalURL)
@@ -102,7 +102,7 @@ func TestBatchOperations(t *testing.T) {
 	ts := setupTestStorage(t)
 	defer ts.cleanup(t)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(t.Context(), 2*time.Second)
 	defer cancel()
 
 	urls := []entity.URL{
@@ -126,7 +126,7 @@ func TestGetUserURLs(t *testing.T) {
 	ts := setupTestStorage(t)
 	defer ts.cleanup(t)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(t.Context(), 2*time.Second)
 	defer cancel()
 
 	_, err := ts.storage.Add(ctx, "user3", "test1", "https://test1.com")
@@ -143,7 +143,7 @@ func TestURLDeletion(t *testing.T) {
 	ts := setupTestStorage(t)
 	defer ts.cleanup(t)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(t.Context(), 2*time.Second)
 	defer cancel()
 
 	shortURL := "delete-test"
@@ -161,7 +161,7 @@ func TestNonExistentURLs(t *testing.T) {
 	ts := setupTestStorage(t)
 	defer ts.cleanup(t)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(t.Context(), 2*time.Second)
 	defer cancel()
 
 	t.Run("get non-existent short URL", func(t *testing.T) {
@@ -179,7 +179,7 @@ func TestPing(t *testing.T) {
 	ts := setupTestStorage(t)
 	defer ts.cleanup(t)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(t.Context(), 2*time.Second)
 	defer cancel()
 
 	err := ts.storage.Ping(ctx)
@@ -196,7 +196,7 @@ func TestStatePreservation(t *testing.T) {
 	storage1, err := file.NewFileStorage(filePath, logger)
 	require.NoError(t, err)
 
-	ctx := context.Background()
+	ctx := t.Context()
 	_, err = storage1.Add(ctx, "user1", "test1", "https://example1.com")
 	require.NoError(t, err)
 	_, err = storage1.Add(ctx, "user1", "test2", "https://example2.com")
@@ -235,7 +235,7 @@ func TestPeriodicSaving(t *testing.T) {
 	)
 	require.NoError(t, err)
 
-	ctx := context.Background()
+	ctx := t.Context()
 	_, err = storage.Add(ctx, "user1", "periodic1", "https://periodic1.com")
 	require.NoError(t, err)
 
